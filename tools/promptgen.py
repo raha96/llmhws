a3fccc4e19d32b971958d1f8b5f89597ef59c953
Raha <raham9619@gmail.com>
2024-03-26 22:19:18 -0600

    Implemented multiple verbosity settings in promptgen

--- tools/promptgen.py
+++ tools/promptgen.py
@@ -72,56 +72,58 @@ if __name__ == "__main__":
     for record in metadata:
         path = record["path"]
         units = record["units"]
+        promptdict = record["prompts"]
         sourcepath = os.path.join(srcroot, path)
         pre_ext, extension = os.path.splitext(sourcepath)
         namegen = name_generator(extension[1:])
-        for unit_info in units:
-            unit = unit_info[0]
-            prompt_header = unit_info[1]
-            prompt_inline = unit_info[2]
+        for verbosity in promptdict:
+            prompt_header = promptdict[verbosity]
+            for unit_info in units:
+                unit = unit_info[0]
+                prompt_inline = unit_info[1]
 
-            # Make the base directory
-            localoutroot = os.path.join(outroot, unit)
-            if os.path.exists(localoutroot):
-                shutil.rmtree(localoutroot)
-            os.mkdir(localoutroot)
-            # Copy the original source files to the destination, excluding SRC
-            shutil.copy(os.path.join(srcroot, "ORIGIN"), localoutroot)
-            shutil.copy(os.path.join(srcroot, "prove.tcl"), localoutroot)
-            shutil.copytree(os.path.join(srcroot, "properties"), os.path.join(localoutroot, "properties"))
-            shutil.copytree(os.path.join(srcroot, "src"), os.path.join(localoutroot, "src"))
+                # Make the base directory
+                localoutroot = os.path.join(outroot, unit + "_" + verbosity)
+                if os.path.exists(localoutroot):
+                    shutil.rmtree(localoutroot)
+                os.mkdir(localoutroot)
+                # Copy the original source files to the destination, excluding SRC
+                shutil.copy(os.path.join(srcroot, "ORIGIN"), localoutroot)
+                shutil.copy(os.path.join(srcroot, "prove.tcl"), localoutroot)
+                shutil.copytree(os.path.join(srcroot, "properties"), os.path.join(localoutroot, "properties"))
+                shutil.copytree(os.path.join(srcroot, "src"), os.path.join(localoutroot, "src"))
 
-            outpath = os.path.join(outroot, "prompt_" + pre_ext.split(os.path.sep)[-1] + "_" + unit + extension)
-            fout = open(outpath, "w")
-            # Print the instruction header
-            fout.write(f"{prompt_header}\r\n\r\n")
-            header_line = namegen.generate_name("HEADER_COMMENT", False)
-            unit_begin_line = namegen.generate_name(unit, True)
-            unit_end_line = namegen.generate_name(unit, False)
-            with open(sourcepath, "r") as fin:
-                state = "header"
-                for line in fin:
-                    _line = line.strip()
-                    if state == "header":
-                        # Purge the header first
-                        if _line == header_line:
-                            state = "normal"
-                    elif state == "unit":
-                        # Wait for the unit to end
-                        if _line == unit_end_line:
-                            state = "normal"
-                    else:
-                        # Keep the lines. Meanwhile, wait for the unit to begin
-                        if _line == unit_begin_line:
-                            # Insert the LLM instruction in lieu of the first line of the unit
-                            fout.write(f"-- Instructions for {llmname}: {prompt_inline}\r\n")
-                            state = "unit"
+                outpath = os.path.join(outroot, "prompt_" + verbosity + "_" + pre_ext.split(os.path.sep)[-1] + "_" + unit + extension)
+                fout = open(outpath, "w")
+                # Print the instruction header
+                fout.write(f"{prompt_header}\r\n\r\n")
+                header_line = namegen.generate_name("HEADER_COMMENT", False)
+                unit_begin_line = namegen.generate_name(unit, True)
+                unit_end_line = namegen.generate_name(unit, False)
+                with open(sourcepath, "r") as fin:
+                    state = "header"
+                    for line in fin:
+                        _line = line.strip()
+                        if state == "header":
+                            # Purge the header first
+                            if _line == header_line:
+                                state = "normal"
+                        elif state == "unit":
+                            # Wait for the unit to end
+                            if _line == unit_end_line:
+                                state = "normal"
                         else:
-                            # Only print the lines that are not LLMHWS-related annotations
-                            if is_annotation.match(line) == None:
-                                fout.write(line)
-            fout.close()
-            outpaths.append(outpath)
+                            # Keep the lines. Meanwhile, wait for the unit to begin
+                            if _line == unit_begin_line:
+                                # Insert the LLM instruction in lieu of the first line of the unit
+                                fout.write(f"-- Instructions for {llmname}: {prompt_inline}\r\n")
+                                state = "unit"
+                            else:
+                                # Only print the lines that are not LLMHWS-related annotations
+                                if is_annotation.match(line) == None:
+                                    fout.write(line)
+                fout.close()
+                outpaths.append(outpath)
     
     print(f"Generated {len(outpaths)} output folders in {outroot}. List: ")
     for path in outpaths:
